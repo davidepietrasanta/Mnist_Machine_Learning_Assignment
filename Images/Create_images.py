c9a456032781e5cf5dd0de9af54f5d6da0269ce2
JackFrost67 <konsegg@live.it>
2020-12-17 17:45:00 +0100

    fix path problem create_image.py script

--- Images/Create_images.py
+++ Images/Create_images.py
@@ -7,16 +7,18 @@ import matplotlib.image as mapl_immage
 from os import listdir
 from os.path import isfile, join
 import pathlib
-mypath = str(pathlib.Path(__file__).parent.absolute()) + "\\"
+mypath = pathlib.Path(__file__).parent.absolute()
 print(mypath)
 
+"""
 onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 print(onlyfiles)
+"""
 
 mnist_image = ['train-images-idx3-ubyte.gz',
             't10k-images-idx3-ubyte.gz']
 
-folder_name = ["Trainset\\", "Testset\\"]
+folder_name = ["Trainset", "Testset"]
 
 len_mnist_image = [60000, 10000]
 
@@ -25,7 +27,7 @@ len_mnist_image = [60000, 10000]
 #Extract image data:
 for j in range(len(mnist_image)):
 
-    f = gzip.open( mypath+mnist_image[j],'r')
+    f = gzip.open( mypath / mnist_image[j],'r')
 
     image_size = 28
     num_images = len_mnist_image[j]
@@ -38,6 +40,6 @@ for j in range(len(mnist_image)):
     #Save images
     data_lenght = len(data)
     for i in range(data_lenght):
-        name_image = mypath+folder_name[j]+str(i+1) + ".png"
+        name_image = mypath / folder_name[j] / pathlib.Path(str(i+1) + ".png")
         image_array = np.asarray(data[i]).squeeze()
         mapl_immage.imsave(name_image, image_array)
