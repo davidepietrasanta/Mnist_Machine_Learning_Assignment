70906c277c35299dcfb843060889f65bfbcd6e14
Davide Pietrasanta <davide.pietrasanta@virgilio.it>
2020-12-24 18:13:17 +0100

    Create the folders if the don't exist

--- Images/Create_images.py
+++ Images/Create_images.py
@@ -4,16 +4,10 @@ from PIL import Image
 import matplotlib.pyplot as plt
 import matplotlib.image as mapl_immage
 
-from os import listdir
-from os.path import isfile, join
+import os
 import pathlib
 mypath = pathlib.Path(__file__).absolute().parents[1]
-print(mypath)
-
-"""
-onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
-print(onlyfiles)
-"""
+print("Actual path: " + str(mypath))
 
 mnist_image = ['MNIST Dataset/train-images-idx3-ubyte.gz',
             'MNIST Dataset/t10k-images-idx3-ubyte.gz']
@@ -37,9 +31,18 @@ for j in range(len(mnist_image)):
     data = np.frombuffer(buf, dtype=np.uint8).astype(np.float32)
     data = data.reshape(num_images, image_size, image_size, 1)
 
+    #Create directory if not exists
+    directory = mypath / "Images"/ folder_name[j]
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
     #Save images
     data_lenght = len(data)
     for i in range(data_lenght):
-        name_image = mypath / "Images"/ folder_name[j] / pathlib.Path(str(i+1) + ".png")
+        name_image = directory / pathlib.Path(str(i+1) + ".png")
         image_array = np.asarray(data[i]).squeeze()
         mapl_immage.imsave(name_image, image_array)
+
+    print(folder_name[j] + " extracted")
+
+print("End")
